1e46632b77b6032d9b9713d5187598167505d80a
wanghaiwen <1192328490@qq.com>
2017-01-03 18:38:43 +0800

    更改数据库为mysql

--- shopsys/settings.py
+++ shopsys/settings.py
@@ -75,8 +75,13 @@ WSGI_APPLICATION = 'shopsys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'shopsys',
+        'USER':'root',
+        'PASSWORD':'123456',
+        'HOST':'',
+        'PORT':'3306',
+
     }
 }
 
@@ -105,7 +110,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
